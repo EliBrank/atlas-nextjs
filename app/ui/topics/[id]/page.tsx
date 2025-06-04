f624dc6aa4c2cbe5d6bf5d0cbdab7a01fbdd834a
eliBrank <ezb.business@pm.me>
2025-06-04 15:32:00 -0500

    make pages dynamic by connecting to database

--- app/ui/topics/[id]/page.tsx
+++ app/ui/topics/[id]/page.tsx
@@ -1,10 +1,35 @@
-export default async function Page({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
+import { AskQuestion } from "@/components/AskQuestion";
+import { Question } from "@/components/Question";
+import { fetchQuestions, fetchTopic } from "@/lib/data";
+import { HashtagIcon } from "@heroicons/react/24/outline";
 
-  // simulate page load
-  // await new Promise((r) => setTimeout(r, 2000));
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const topic = await fetchTopic(id);
+  const questions = await fetchQuestions(id);
+
+  if (!topic) {
+    return <div>Topic not found</div>;
+  }
 
   return (
-    <div>Topic Page: {id}</div>
+    <div>
+      <h1 className="flex items-center text-3xl font-black">
+        <HashtagIcon className="mr-2 h-6 w-6" /> {topic.title}
+      </h1>
+      <AskQuestion topic={topic.id} />
+      {questions.map((question) => (
+        <Question
+          key={question.id}
+          id={question.id}
+          text={question.title}
+          votes={question.votes}
+        />
+      ))}
+    </div>
   );
 }
