ac67b71aecf0cd3e91ccc4086b5892e911ccffcd
eliBrank <ezb.business@pm.me>
2025-06-04 13:45:58 -0500

    adjust existing components to use nextjs optimized replacements, specify use client for client-side rendering

--- components/Sidenav.tsx
+++ components/Sidenav.tsx
@@ -4,12 +4,13 @@ import TopicLinks from "./TopicLinks";
 import NavLink from "./NavLink";
 import SignOutButton from "./SignOutButton";
 import NewTopicButton from "./NewTopicButton";
+import Link from "next/link";
 
 export default async function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Logo />
-      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2 overflow-scroll">
+      <div className="flex grow flex-row justify-between space-x-2 overflow-scroll md:flex-col md:space-y-2 md:space-x-0">
         <NavLink name="Topics" href="/ui" />
         <TopicLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
@@ -22,15 +23,15 @@ export default async function SideNav() {
 
 function Logo() {
   return (
-    <a
+    <Link
       className="mb-2 flex h-20 items-end justify-center rounded-md bg-secondary p-4 md:h-40"
       href="/ui"
     >
       <Image
         src={logo}
         alt="Acme Logo"
-        className="h-14 md:h-full object-contain"
+        className="h-14 object-contain md:h-full"
       />
-    </a>
+    </Link>
   );
 }
