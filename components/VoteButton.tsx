b8b700b04c374138186d1ac413e8d6dd819127b0
eliBrank <ezb.business@pm.me>
2025-06-04 22:22:56 -0500

    set up form actions, pass in hidden values

--- components/VoteButton.tsx
+++ components/VoteButton.tsx
@@ -1,11 +1,13 @@
 import { HandThumbUpIcon } from "@heroicons/react/24/outline";
+import { addVote } from "@/lib/actions";
 
 export default function VoteButton({ id }: { id: string }) {
   return (
-    <form>
+    <form action={addVote}>
+      <input type="hidden" name="id" value={id} className="hidden" />
       <button
         type="submit"
-        className="h-8 w-8 min-w-[2rem] rounded-full ring-gray-200 hover:text-atlas-teal active:bg-primary active:text-white active:outline-hidden active:ring-2 active:ring-primary"
+        className="hover:text-atlas-teal h-8 w-8 min-w-[2rem] rounded-full ring-gray-200 active:bg-primary active:text-white active:ring-2 active:ring-primary active:outline-none"
       >
         <HandThumbUpIcon />
       </button>
