816f645e6483c4dfa5dc754564a6f27ec40807a5
eliBrank <ezb.business@pm.me>
2025-06-04 23:44:11 -0500

    add auth, login functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import placeholder from "@/assets/placeholder.svg";
+import { signIn } from "@/auth";
 
 export default function Page() {
   return (
@@ -16,7 +17,12 @@ export default function Page() {
               scale your web applications with ease.
             </p>
             <div className="flex flex-col gap-2 min-[400px]:flex-row">
-              <form>
+              <form
+                action={async () => {
+                  "use server"
+                  await signIn("default", { redirectTo: "/ui" });
+                }}
+              >
                 <button className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors focus-visible:ring-1 focus-visible:ring-ring focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50">
                   <div>Sign In</div>
                 </button>
